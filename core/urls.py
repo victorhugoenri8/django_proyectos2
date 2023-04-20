af1963ec7948cf30cefd5dd497f3419b73b4e7cb
victorenri8 <victorhugoenriquezco@gmail.com>
2023-04-19 22:24:35 -0500

    se agrego el modulo blog y crear post desde admin

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
+    path('blog/', include('blog.urls', namespace='blog'))
 ]
